c770ad2df29a813960fa19401833bb6501e1c450
Altran Batista da Costa <143886690+AltranCosta@users.noreply.github.com>
2026-01-29 20:44:20 +0000

    Apredendo switch case

---
+++ switch case/Program.java
@@ -0,0 +1,41 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um Numero de 1 ate 7 para sabe qual e o dia da semana: ");
+        int x = sc.nextInt();
+        String dia;
+
+        switch (x){
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda";
+                break;
+            case 3:
+                dia = "Terça";
+                break;
+            case 4:
+                dia = "Quarta";
+                break;
+            case 5:
+                dia = "Quinta";
+                break;
+            case 6:
+                dia = "Sexta";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                dia = "Valor Invalido";
+        }
+        System.out.println("Dia da semana é: " + dia);
+        sc.close();
+    }
+
+}
